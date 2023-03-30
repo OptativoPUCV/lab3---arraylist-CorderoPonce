b192f560103a91feadd4c3a7ba420e7dbc92e8a2
Replit user <>
2023-03-30 23:59:35 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -103,6 +103,6 @@ int get_size(ArrayList * l){
 void clean(ArrayList * l){
   l->capacity = 2;
   l->size = 0;
-  l = realloc (l, sizeof(int) * 2);
+  l->data = realloc (l->data, sizeof(int) * 2);
     
 }
